7a0c58be5f09db0a367bdd2606a249efc5bdebbf
Philip Lindberg <philip.c.lindberg@gmail.com>
2022-12-21 19:30:15 +0100

    Start android implementation

--- android/src/main/java/expo/modules/toast/ToastModule.kt
+++ android/src/main/java/expo/modules/toast/ToastModule.kt
@@ -1,19 +1,30 @@
 package expo.modules.toast
 
-import android.widget.Toast
+import android.graphics.Color
+import com.google.android.material.snackbar.BaseTransientBottomBar.ANIMATION_MODE_SLIDE
+import com.google.android.material.snackbar.Snackbar
 import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
+import expo.modules.kotlin.types.Enumerable
 
 class ToastModule : Module() {
-  private val context
-  get() = requireNotNull(appContext.reactContext)
+  private val rootView
+  get() = requireNotNull(appContext.activityProvider?.currentActivity?.window?.decorView?.rootView)
 
   override fun definition() = ModuleDefinition {
     Name("Toast")
 
-    Function("show") { text: String ->
-      // TODO: implement as snackbar: https://developer.android.com/reference/com/google/android/material/snackbar/Snackbar
-      Toast.makeText(context, text, Toast.LENGTH_LONG).show()
+    Function("show") { text: String, type: ToastType?, duration: Int ->
+      Snackbar.make(rootView, text, duration * 1000)
+        .setTextColor(Color.parseColor("#FFFFFF"))
+        .setAnimationMode(ANIMATION_MODE_SLIDE)
+        .show()
     }
   }
+}
+
+enum class ToastType(val value: String) : Enumerable {
+  SUCCESS("success"),
+  ERROR("error"),
+  INFO("info")
 }
